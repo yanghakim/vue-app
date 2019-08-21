6252b0221e60dca73603cb43b8b6aeb358a4d69a
yanghakim <yanghakim0@gmail.com>
2019-08-21 12:20:47 -0400

    fixed build problem

--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import todos from "./modules/todos";
 
 // Load Vuex
 Vue.use(Vuex);
 
 // Create Store
-export default new Vuex.Store({
-  modules: { todos }
-});
+export default new Vuex.Store({});
